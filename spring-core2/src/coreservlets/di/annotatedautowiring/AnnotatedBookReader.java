2f4d62dc1d71c5869b2b9bb8caeaf326cdc5a3df
LENOVO\USER <ananthkec@gmail.com>
2017-09-23 09:12:12 +0530

    Spring core - 2

---
+++ spring-core2/src/coreservlets/di/annotatedautowiring/AnnotatedBookReader.java
@@ -0,0 +1,34 @@
+package coreservlets.di.annotatedautowiring;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import coreservlets.Book;
+import coreservlets.BookLibrary;
+
+public class AnnotatedBookReader {
+
+  @Autowired //byName - default
+  private BookLibrary bookLibrary;
+  
+  public AnnotatedBookReader(){
+  }
+
+  public AnnotatedBookReader(BookLibrary bookLibrary) {
+    this.bookLibrary = bookLibrary;
+  }
+
+  //@Autowired //byType
+  public void setBookLibrary(BookLibrary bookLibrary) {
+    this.bookLibrary = bookLibrary;
+  }
+
+  public List<Book> read() {
+    List<Book> books = bookLibrary.search("Java");
+    for(Book book : books){
+      System.out.printf("Reading: %s%n", book);
+    }
+    return books;
+  }
+}
